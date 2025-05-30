628ab4cd7f435e6998aedfb5481b784e03eea3d3
Manish Dhubkarya <147823856+Manish-Dhubkarya@users.noreply.github.com>
2025-05-30 12:28:25 +0530

    changes

--- src/sections/auth/sign-in-view.tsx
+++ src/sections/auth/sign-in-view.tsx
@@ -266,9 +266,9 @@ export function SignInView() {
             color: 'text.secondary',
           }}
         >
-          Don't have an account?{' '}
+          Don't have an account? 
           <Link variant="subtitle2" sx={{ ml: 0.5, cursor: 'pointer' }} onClick={handleSignUpClick}>
-            Sign up
+             Sign up
           </Link>
         </Typography>
       </Box>
